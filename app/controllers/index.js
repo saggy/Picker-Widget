37cf9769b8f96c0f9df3debe63e2e474d4c8965e
Sagar Patel <saggy_6161@ymail.com>
2014-06-04 17:46:50 +0530

    Change the FontSize in Picker
    Change the FontSize in Picker

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,24 +1,26 @@
 var data = [], pId;
 
-function openPicker(e){
-	for(var i=0; i<10; i++){
-		data.push(i.toString());
+function openPicker(e) {
+	for (var i = 0; i < 10; i++) {
+		data.push({
+			picValue : i.toString(),
+			fontSize : 30
+		});
 	}
 	var wid = Alloy.createWidget("pickerWidget");
-	 pId = wid.getView();
-	 $.index.add(pId);
-	 wid.init({
-	 	value : data,
-	 	doneCallback : function(e){
-	 		Ti.API.info("doneCallback data E | " + JSON.stringify(e));
-	 		$.lbl.text = e.row.title;
-	 	}, 
-	 	cancelCallback : function(e){
-	 		Ti.API.info("cancelCallback");
-	 	}
-	 });
-	 data = [];
+	pId = wid.getView();
+	$.index.add(pId);
+	wid.init({
+		value : data,
+		doneCallback : function(e) {
+			Ti.API.info("doneCallback data E | " + JSON.stringify(e));
+			$.lbl.text = e.row.title;
+		},
+		cancelCallback : function(e) {
+			Ti.API.info("cancelCallback");
+		}
+	});
+	data = [];
 }
-
 
 $.index.open();
--- app/widgets/pickerWidget/controllers/widget.js
+++ app/widgets/pickerWidget/controllers/widget.js
@@ -1,17 +1,19 @@
 var ARG = arguments[0] || {};
-var dataReview = [], rowData, value=[];
+var dataReview = [], rowData, value = [];
 $.done, $.cancel;
 
-$.init = function(params){
-	Ti.API.info('value-->'+params.value);
-	if(params.value.length){
+$.init = function(params) {
+	Ti.API.info('value-->' + JSON.stringify(params.value));
+	
+	if (params.value.length) {
 		Object.keys(params.value).forEach(function(pValue) {
 			dataReview.push(Ti.UI.createPickerRow({
-				title : params.value[pValue]
+				title : params.value[pValue].picValue,
+				fontSize :params.value[pValue].fontSize
 			}));
 		});
 	}
-	if(dataReview.length){
+	if (dataReview.length) {
 		$.picker.add(dataReview);
 	}
 	$.done = params.doneCallback;
@@ -19,30 +21,30 @@ $.init = function(params){
 };
 
 // on changeEvent of picker
-$.picker.addEventListener('change', function(e){
-	Ti.API.info('title-->'+ e.row.title);
+$.picker.addEventListener('change', function(e) {
+	Ti.API.info('title-->' + e.row.title);
 	rowData = e;
 });
 
 // cancel button
-$.cancelBtn.addEventListener("click",function(e){
+$.cancelBtn.addEventListener("click", function(e) {
 	closeAndSavePicker($.parentView);
 });
 
 //done button
-$.doneBtn.addEventListener("click",function(e){
+$.doneBtn.addEventListener("click", function(e) {
 	closeAndSavePicker($.parentView);
 	$.done(rowData);
 });
 
 //To remove picker from its parent container
-function closeAndSavePicker(viewObject){
-	dataReview = [], value=[];
+function closeAndSavePicker(viewObject) {
+	dataReview = [], value = [];
 	viewObject.hide();
 	var children = viewObject.children.slice(0);
 	for (var i = 0; i < children.length; ++i) {
 		viewObject.remove(children[i]);
 	}
 	var parent = viewObject.getParent();
-		parent.remove(viewObject);
+	parent.remove(viewObject);
 };
